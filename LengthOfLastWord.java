5d279bc9c4cdec93d8a5e01b8a5072c874b8c8ea
saloni kumawat <545saloni@gmail.com>
2021-04-11 13:08:53 +0530

    Length of the last word

---
+++ LengthOfLastWord.java
@@ -0,0 +1,52 @@
+/**
+ * https://leetcode.com/problems/length-of-last-word/
+ * 
+ * Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+ * 
+ * A word is a maximal substring consisting of non-space characters only.
+ * 
+ *  
+ * 
+ * Example 1:
+ * 
+ * Input: s = "Hello World"
+ * Output: 5
+ * Example 2:
+ * 
+ * Input: s = " "
+ * Output: 0
+ *  
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 104
+ * s consists of only English letters and spaces ' '
+ */
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        LengthOfLastWord solution = new LengthOfLastWord();
+        String s = "Hello World";
+        System.out.println(solution.lengthOfLastWord(s));
+
+    }
+    
+    /**
+     * Find Length of last word of the input string.
+     * 
+     * Steps:
+     * 1. Start a loop i from s.length to 0 and do i--
+     * 2. Check if s[i] is equal to " " (space), return (s.length - i -1).
+     * 3. At the end of loop return s.length.
+     * 
+     * @param str is input string
+     * @return count of length of last word of string.
+     */
+    public int lengthOfLastWord(String str) { // O(n), O(1) where n is length of input string
+        int length = str.length(); // O(1), O(1)
+        for(int i = length - 1; i >= 0 ; i--) { // O(n), O(1)
+            if(str.charAt(i) == ' ') return length - i - 1; // O(1), O(1)
+        }
+        return length; // O(1), O(1)
+    }
+
+}
